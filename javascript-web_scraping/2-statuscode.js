5effe6d246bdd136f727161b5e36fd9ed76ccff1
omda3310 <6197@holbertonstudents.com>
2023-10-14 16:57:13 +0200

    Status code

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const request = require('request');
-const url = process.argv[2]; 
+const url = process.argv[2];
 
-request(url, function(error, response) {
-  if(error) {
+request(url, function (error, response) {
+  if (error) {
     console.error(error);
   } else {
     console.log('code:', response.statusCode);
