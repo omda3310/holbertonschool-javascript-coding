34a14f868d9fe92c46965398b2e47b799bad8e01
omda3310 <6197@holbertonstudents.com>
2023-10-14 17:37:22 +0200

    Star wars Wedge Antilles

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const request = require('request');
+const apiUrl = 'https://swapi-api.hbtn.io/api/films/';
+let count = 0;
+
+request(apiUrl, function(error, response, body) {
+    if (error) {
+        console.error(error);
+    } else {
+        const films = JSON.parse(body).results;
+        films.forEach(function(film) {
+            if (film.characters.includes("https://swapi-api.hbtn.io/api/people/18/")) {count++;}
+        });
+    }
+    console.log(count);
+});
