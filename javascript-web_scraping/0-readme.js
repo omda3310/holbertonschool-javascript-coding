906eb89c9ec579aa022fc03f01ecda3425e9706a
omda3310 <6197@holbertonstudents.com>
2023-10-14 12:45:16 +0200

    Read content of a file

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+/* File System Object */
+var fs = require('fs');
+const argv = process.argv[2];
+
+/* Read File */
+fs.readFile(fileToRead, 'utf8', (err, data) => {
+    if(err) {
+      console.log(err.message);
+  } else {
+      console.log(data);
+  }
+  });
