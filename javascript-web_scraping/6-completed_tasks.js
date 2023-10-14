4f4f3889574b2484669b237ec350e870c7b10fb4
omda3310 <6197@holbertonstudents.com>
2023-10-15 00:07:36 +0200

    completed_tasks

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -9,17 +9,15 @@ const completedTasks = {};
 request(apiUrl, function (error, response, body) {
   if (error) {
     console.error(error);
-  }
-  else {
+  } else {
     const todos = JSON.parse(body);
 
     for (const todo of todos) {
       if (todo.completed) {
         if (completedTasks[todo.userId]) {
           completedTasks[todo.userId]++;
-        }
-        else {
-          completedTasks[todo.userId] = 1;
+        } else {
+            completedTasks[todo.userId] = 1;
         }
       }
     }
