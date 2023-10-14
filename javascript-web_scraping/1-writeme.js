170dc9248b3f7949519f7467b3ab4827c1f708c7
omda3310 <6197@holbertonstudents.com>
2023-10-14 13:28:53 +0200

    writes a string to a file

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 const f = require('fs');
-const { argv } = require('process');
-const File_path = process.argv[2];
+const file_path = process.argv[2];
 const content = process.argv[3];
 
-f.writeFile(File_path, content, function(error) {
-    if(error) {
-        console.log(error);
-    }
+f.writeFile(file_path, content, function (error) {
+  if (error) {
+    console.log(error);
+  }
 });
