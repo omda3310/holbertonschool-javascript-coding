65b37324cf206cd8e0166d93fd36be1af1e37b71
omda3310 <6197@holbertonstudents.com>
2023-10-14 13:05:42 +0200

    Read content of a file

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
-const { error } = require('console');
+
 const fs = require('fs');
 const argv = process.argv[2];
 
 /* Read File */
-fs.readFile(argv, 'utf-8', function(error, data) {
-    if(error) {
-      console.error(error);
+fs.readFile(argv, 'utf-8', function (error, data) {
+  if (error) {
+    console.error(error);
   } else {
-      console.log(data);
+    console.log(data);
   }
-  });
+});
